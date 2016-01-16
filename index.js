f2eca653072a756217d0592e748bf891551b92b4
Michael Mroz <michael@mroz.io>
2016-01-17 04:15:30 +1100

    Sweet dreams are made of memes

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const uuid = require('uuid');
 const app = express();
 const users = {};
 
-const Parse = require('parse').Parse;
+const Parse = require('parse/node').Parse;
 Parse.initialize("eT8HUCePhitJSxgQSQOjRFko38GIbNUJu9AxNGeE",
-    "NaDZMVWeFm79BBaim8nd0yqtyHJTDww4Ne2Ww6T5");
-
+    "NaDZMVWeFm79BBaim8nd0yqtyHJTDww4Ne2Ww6T5",
+    "sYmBU9QT5ziNUeEkbc5I6rdi3BanuBItugGCfJu5");
 
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({extended: true}) );
@@ -21,15 +21,24 @@ app.set('port', (process.env.PORT || 5000));
 
 //placeholder get
 app.get('/', (req, res) => {
-    res.send({'memes':true});
+    var ok = (result) => {
+        res.send(result);
+    };
     Parse.Push.send({
-        data: { }
+        where: {
+            deviceType: "android"
+        },
+        data: {
+            "alert": "Sweet dreams are made of memes"
+        }
     }, {
         success: function() {
             // Push was successful
+            ok("Sweet memes are made of dreams");
         },
         error: function(error) {
             // Handle error
+            ok(error);
         }
     });
 });
