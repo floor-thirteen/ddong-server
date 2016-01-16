71683e8d4a31c52ade5c37914d78e6482f5a0a11
Michael Mroz <michael@mroz.io>
2016-01-17 03:34:36 +1100

    Champion GETs

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const uuid = require('uuid');
 const app = express();
 const users = {};
 
+const Parse = require('parse').Parse;
+Parse.initialize("eT8HUCePhitJSxgQSQOjRFko38GIbNUJu9AxNGeE",
+    "NaDZMVWeFm79BBaim8nd0yqtyHJTDww4Ne2Ww6T5");
+
+
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({extended: true}) );
 
@@ -17,6 +22,16 @@ app.set('port', (process.env.PORT || 5000));
 //placeholder get
 app.get('/', (req, res) => {
     res.send({'memes':true});
+    Parse.Push.send({
+        data: { }
+    }, {
+        success: function() {
+            // Push was successful
+        },
+        error: function(error) {
+            // Handle error
+        }
+    });
 });
 
 app.post('/', (req, res) => {
