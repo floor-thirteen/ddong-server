a04e32fd948dd20547b8859a17d5c267a07c6465
tristanhessell <tristanhessell@gmail.com>
2016-01-17 00:59:27 +1100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+/* jshint esnext: true, node:true */
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+    //
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Node app is running on port ${app.get('port')}`);
+});
